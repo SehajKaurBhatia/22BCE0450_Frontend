162d08bdedfcf51cacf1a8caf83d11e9fbc6e2a0
Sehaj <114878033+SehajKaurBhatia@users.noreply.github.com>
2025-03-30 19:02:40 +0530

    Update navigation.tsx

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,13 +1,1 @@
-import React from 'react'
-import Link from 'next/link'
-const navigation = () => {
-  return (
-    <div>
-        <Link href="/">Home</Link>
-        <Link href="/Products/1">Products</Link>
-        
-    </div>
-  )
-}
 
-export default navigation
