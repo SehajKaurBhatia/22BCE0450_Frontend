0e0ade5d43ec3e39bf84a2c533cceeb404c65c81
Sehaj <114878033+SehajKaurBhatia@users.noreply.github.com>
2025-03-30 19:02:24 +0530

    Update Toggle.tsx

--- src/app/components/Toggle.tsx
+++ src/app/components/Toggle.tsx
@@ -7,16 +7,16 @@ const Toggle=()=> {
 
   return (
     <div className="text-[14px] ">
-    {/* Toggle Button Inside the Empty div */}
+   
     <div className="relative flex items-center justify-between w-auto h-[50px] bg-gray-200 rounded-[11px] px-1 cursor-pointer">
-      {/* Sliding Background */}
+     
       <motion.div
         className="absolute left-2 right-2 w-[110px] h-[36px] bg-white rounded-[11px] shadow-md"
         animate={{ x: isGridView ? 0 : 122 }} 
         transition={{ type: "spring", stiffness: 200, damping: 20 }}
       />
 
-      {/* Grid View Button */}
+     
       <button
         className="cursor-pointer relative z-10 font-bold w-[120px] h-[36px] flex items-center justify-center "
         onClick={() => setIsGridView(true)}
@@ -24,7 +24,7 @@ const Toggle=()=> {
         Grid View
       </button>
 
-      {/* List View Button */}
+     
       <button
         className="cursor-pointer relative z-10 font-bold w-[120px] h-[36px] flex items-center justify-center  "
         onClick={() => setIsGridView(false)}
